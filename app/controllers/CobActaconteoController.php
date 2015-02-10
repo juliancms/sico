d37af397fa67fd3ca92cf79926a264769eda4f10
juliancms <marin.julian@gmail.com>
2015-02-10 08:20:50 -0500

    Test error adicionales

--- app/controllers/CobActaconteoController.php
+++ app/controllers/CobActaconteoController.php
@@ -323,7 +323,7 @@ class CobActaconteoController extends ControllerBase
     	$tipos = array("image/png", "image/jpeg", "image/jpg", "image/bmp", "image/gif");
     	if ($this->request->isPost()) {
     		if ($this->request->hasFiles() == true) {
-    			echo "Hola";
+    			echo "Error";
 	    		$uploads = $this->request->getUploadedFiles();
 	    		$isUploaded = false;
 	    		foreach($uploads as $upload){
