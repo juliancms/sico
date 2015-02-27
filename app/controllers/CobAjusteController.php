64c77e38e45a07ce90754600dc159bca397c3b04
Julián C. Marín S. <marin.julian@gmail.com>
2015-02-27 16:06:00 -0500

    Modificación opción borrar ajuste

--- app/controllers/CobAjusteController.php
+++ app/controllers/CobAjusteController.php
@@ -24,6 +24,7 @@ class CobAjusteController extends ControllerBase
             $this->flash->notice("No se ha agregado ningún ajuste hasta el momento");
             $cob_ajuste = null;
         }
+        $this->view->nivel = $this->user['nivel'];
         $this->view->cob_ajuste = $cob_ajuste;
     }
     
