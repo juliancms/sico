05d186a4dc9b3f1bc879bf5002f4be3f53cf400d
Julián C. Marín S. <marin.julian@gmail.com>
2015-03-03 00:41:43 -0500

    Test 4 bug enlace GI

--- app/controllers/IbcGestionInstitucionalController.php
+++ app/controllers/IbcGestionInstitucionalController.php
@@ -19,7 +19,7 @@ class IbcGestionInstitucionalController extends ControllerBase
     public function indexAction()
     {
         $this->persistent->parameters = null;
-        echo $this->conversiones->get_client_ip; break;
+        echo $this->conversiones->get_client_ip(); break;
         $menu = GinOferenteMenu::findFirstByid_usuario($this->id_usuario);
         if ($_SERVER["REMOTE_ADDR"] == "http://190.248.150.222"){
         	$this->view->url = "http://190.248.150.222:347/owncloud/" . $menu->menu;
