e6860278d3e8a3adf887f247804ef612ebdd41de
Julián C. Marín S. <marin.julian@gmail.com>
2015-05-27 14:32:04 -0500

    Corrección instrumentos

--- app/controllers/IbcInstrumentosController.php
+++ app/controllers/IbcInstrumentosController.php
@@ -22,7 +22,7 @@ class IbcInstrumentosController extends ControllerBase
         if (substr($this->conversiones->get_client_ip(), 0, 7) == "192.168"){
         	$this->view->url = "http://192.168.2.4/owncloud/index.php/s/Uw3983BdeJnuUfl";
         } else {
-        	$this->view->url = "http://190.248.150.222:347/owncloud/index.php/s/Uw3983BdeJnuUfl";
+        	$this->view->url = "http://190.248.150.222:842/owncloud/index.php/s/Uw3983BdeJnuUfl";
         }
     }
 
