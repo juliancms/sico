0562106a5dfbc0309398d921cb0292a60c0b4ca6
juliancms <marin.julian@gmail.com>
2016-02-11 07:03:53 -0500

    Archivo Digital

--- app/controllers/IbcArchivoDigitalController.php
+++ app/controllers/IbcArchivoDigitalController.php
@@ -19,7 +19,7 @@ class IbcArchivoDigitalController extends ControllerBase
     public function indexAction($anio)
     {
         $this->persistent->parameters = null;
-        $menu = BcOferenteMenu::find("id_usuario = $this->id_usuario AND anio = $anio");
+        $menu = BcOferenteMenu::find("id_usuario = $this->id_usuario AND anio = $anio")->toArray();
         if($menu){
         	if (substr($this->conversiones->get_client_ip(), 0, 7) == "192.168"){
         		$this->view->url = "http://192.168.2.4/owncloud/" . $menu[0]->menu;
