fb5585687a747f97e49bf8e8164ce5378a8ca10b
Julián Marín <marin.julian@gmail.com>
2016-07-22 11:15:28 -0500

    Adición de madres comunitarias carga e impresión ECI

---
+++ app/models/CobActaconteoMcb.php
@@ -0,0 +1,46 @@
+<?php
+
+class CobActaconteoMcb extends \Phalcon\Mvc\Model
+{
+    /**
+     *
+     * @var integer
+     */
+    public $id_actaconteo_mcb;
+
+    /**
+     *
+     * @var integer
+     */
+    public $id_actaconteo;
+
+    /**
+     *
+     * @var integer
+     */
+    public $numDocumento;
+
+    /**
+     *
+     * @var string
+     */
+    public $primerNombre;
+
+    /**
+     *
+     * @var string
+     */
+    public $segundoNombre;
+
+    /**
+     *
+     * @var string
+     */
+    public $primerApellido;
+
+    /**
+     *
+     * @var string
+     */
+    public $segundoApellido;
+}
